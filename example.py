9731b4bc52e9672d5081d08c7c21cc3f9e41cd67
JustAntoRS <justantorami@gmail.com>
2019-10-16 13:41:14 +0200

    Improved example.py

--- example.py
+++ example.py
@@ -1,22 +1,46 @@
 import os
 from game_of_life.map import Map
 
-m = Map(20, 20)
-
-m.getCell(5,5).changeState()
-m.getCell(5,6).changeState()
-m.getCell(6,6).changeState()
-m.getCell(6,7).changeState()
-m.getCell(7,6).changeState()
-
 os.system('cls' if os.name == 'nt' else 'clear')
+
+#Map creation
+row_num = int(input('Choose the number of rows of the cell matrix: '))
+column_num = int(input('Choose the number of columns of the cell matrix: '))
+print('This is your matrix:')
+m = Map(row_num, column_num)
 m.print()
-while(1):
-    answer = input("Next iteration? yes/no ")
-    if answer == 'yes' or answer == 'y':
-            os.system('cls' if os.name == 'nt' else 'clear')
-            m.update()
-            m.print()
-    elif answer == 'no' or answer == 'n':
+
+input('Press something to continue')
+os.system('cls' if os.name == 'nt' else 'clear')
+#Init cells in gen 0
+while 1:
+    m.print()
+    print('Choose which cells should be alive in generation 0')
+    print('Format: 0 < row_position < ' + str(row_num) + ', 0 < column_position <' + str(column_num))
+    print('Example: 5,6 represents Cell at position matrix[5][6]')
+    print('When you are done creating born cells, write DONE')
+    inp = input()
+    #Check if user is done changing cells
+    if inp == 'done' or inp == 'DONE':
+        break
+
+    #Get list of input
+    cell = inp.split(',')
+
+    #Check if the list is empty
+    if len(cell) != 2:
         os.system('cls' if os.name == 'nt' else 'clear')
-        exit()
+        print("INVALID FORMAT!")
+    else:
+        m.getCell(int(cell[0]),int(cell[1])).changeState()
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+while 1:
+    os.system('cls' if os.name == 'nt' else 'clear')
+    m.update()
+    m.print()
+    inp = input('Next Generation? [yes/no]')
+    if inp == 'y' or inp == 'yes':
+        continue
+    else:
+        exit(0)
